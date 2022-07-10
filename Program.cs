40634275da5d274a2dacc3fa62b1439d58012e9e
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-10 17:28:00 +0300

    2
    Поправил: minDegreeOfTwo - называйте так, чтобы можно было заменить 2 на любое другое значение и для этого поменяется только значение одной переменной, а не имена переменных. 2. "для нахождения минимальной степени 2" - вот и здесь, у вас есть переменная, используйте её для вывода значения

--- Program.cs
+++ Program.cs
@@ -1,28 +1,27 @@
-﻿
+
 namespace JuniorHomeWork
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int minDegreeOfTwo;
+            int minDegree;
             int degree = 0;
             int baseNumber = 2;
             int minNumber = 1;
             int maxNumber = 1000;
-            
+
             Random random = new Random();
             int number = random.Next(minNumber, maxNumber);
 
-            Console.WriteLine("Случайное число равно " + number + " для нахождения минимальной степени 2, превосходящей данное число нажмите любую клавишу ");
+            Console.WriteLine("Случайное число равно " + number + " для нахождения минимальной степени " + baseNumber + ", превосходящей данное число нажмите любую клавишу ");
             Console.ReadLine();
 
             while (Math.Pow(baseNumber, degree) <= number)
             {
                 degree++;
-                minDegreeOfTwo = Convert.ToInt32(Math.Pow(baseNumber, degree));
+               minDegree = Convert.ToInt32(Math.Pow(baseNumber, degree));
             }
-            
             Console.WriteLine("Минимальная степень равна " + degree);
 
         }
